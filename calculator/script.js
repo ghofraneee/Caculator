b27ca18f6acd160f32ea5abc4bbd676bef353231
GHOFRANE CHEMNANE <111186011+ghofraneee@users.noreply.github.com>
2025-07-14 16:42:15 +0100

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,37 @@
+
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll("button");
+//stor
+
+    let input = "";
+
+    buttons.forEach(btn => {
+      btn.addEventListener("click", () => {
+        const value = btn.innerText;
+
+        if (value === "=") {
+          try {
+            input = eval(input.replace(/x/g, "*"));
+          } catch {
+            input = "Error";
+          }
+        } else if (value === "RESET") {
+          input = "";
+        } else if (value === "DEL") {
+          input = input.slice(0, -1);
+        } else {
+          input += value;
+        }
+
+        /*display.innerText = input;
+        if (input.length > 20) {
+          display.innerText = "Input too long";
+          input = "";
+        }
+        if (input === "Error") {
+          display.innerText = "Error";
+          input = "";
+        }*/
+
+      });
+    });
