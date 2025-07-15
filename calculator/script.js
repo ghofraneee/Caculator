516aff852b1bef4ab2f78dc3df4db9d76e47b07b
GHOFRANE CHEMNANE <111186011+ghofraneee@users.noreply.github.com>
2025-07-15 11:12:33 +0100

    Update final script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,29 +1,25 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
 
-    const display = document.getElementById("display");
-    const buttons = document.querySelectorAll("button");
-//stor
+let input = "";
 
-    let input = "";
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const value = btn.innerText;
 
-    buttons.forEach(btn => {
-      btn.addEventListener("click", () => {
-        const value = btn.innerText;
-
-        if (value === "=") {
-          try {
-            input = eval(input.replace(/x/g, "*"));
-          } catch {
-            input = "Error";
-          }
-        } else if (value === "RESET") {
-          input = "";
-        } else if (value === "DEL") {
-          input = input.slice(0, -1);
-        } else {
-          input += value;
-        }
-
-    
-
-      });
-    });
+    if (value === "=") {
+      try {
+        input = eval(input.replace(/x/g, "*"));
+      } catch {
+        input = "Error";
+      }
+    } else if (value === "RESET") {
+      input = "";
+    } else if (value === "DEL") {
+      input = input.slice(0, -1);
+    } else {
+      input += value;
+    }
+    display.innerText = input;
+  });
+});
